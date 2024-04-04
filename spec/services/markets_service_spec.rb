10a5f2241b1cfd43f16dc17ed5a1e303dcc07ab8
Odell McFarland IV <odell.mac4@gmail.com>
2024-04-04 00:18:56 -0700

    Test: returns market by id from market show endpoint

--- spec/services/markets_service_spec.rb
+++ spec/services/markets_service_spec.rb
@@ -21,9 +21,7 @@ RSpec.describe MarketsService, :vcr do
     market = markets[:data].first
     expect(market).to have_key :attributes
     expect(market[:attributes]).to be_a Hash
-    expect(market[:attributes]).to have_key :name
-    expect(market[:attributes][:name]).to be_a String
-
+    
     expect(market[:attributes]).to have_key :name
     expect(market[:attributes][:name]).to be_a String
 
@@ -50,5 +48,42 @@ RSpec.describe MarketsService, :vcr do
 
     expect(market[:attributes]).to have_key :vendor_count
     expect(market[:attributes][:vendor_count]).to be_an Integer
+  end
+
+  it 'returns a market by id' do
+    market = @service.market_by_id("322458")
+
+    expect(market).to be_a Hash
+    expect(market[:data]).to be_a Hash
+    expect(market[:data]).to have_key :id
+    expect(market[:data]).to have_key :attributes
+
+    market_data = market[:data]
+    expect(market_data[:attributes]).to have_key :name
+    expect(market_data[:attributes][:name]).to be_a String
+
+    expect(market_data[:attributes]).to have_key :street
+    expect(market_data[:attributes][:street]).to be_a String
+
+    expect(market_data[:attributes]).to have_key :city
+    expect(market_data[:attributes][:city]).to be_a String
+
+    expect(market_data[:attributes]).to have_key :county
+    expect(market_data[:attributes][:county]).to be_a String
+
+    expect(market_data[:attributes]).to have_key :state
+    expect(market_data[:attributes][:state]).to be_a String
+
+    expect(market_data[:attributes]).to have_key :zip
+    expect(market_data[:attributes][:zip]).to be_a String
+
+    expect(market_data[:attributes]).to have_key :lat
+    expect(market_data[:attributes][:lat]).to be_a String
+
+    expect(market_data[:attributes]).to have_key :lon
+    expect(market_data[:attributes][:lon]).to be_a String
+
+    expect(market_data[:attributes]).to have_key :vendor_count
+    expect(market_data[:attributes][:vendor_count]).to be_an Integer
   end
 end
